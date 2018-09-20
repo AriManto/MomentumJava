3ad7b8fc737da16e5b83dcaede51115e3d95cd95
Fernando <fernandolamasw@hotmail.com>
2018-09-19 22:18:14 -0300

    Ejercicios de procedimientos
    Nuevos ejercicios!

--- Ejercicio 1/KioskoDenuevo/src/kioskodenuevo/KioskoDenuevo.java
+++ Ejercicio 1/KioskoDenuevo/src/kioskodenuevo/KioskoDenuevo.java
@@ -6,6 +6,14 @@ public class KioskoDenuevo {
 
     public static void main(String[] args) {
 
+        /*1) Se necesita un sistema para que un kiosco administre sus
+productos:
+*/
+        
+        /*c) Crear un subprograma que permita adicionar una cantidad
+de Productos provistos por el usuario a una
+ListaDeProductos.*/
+        
     }
 
 }
---
+++ Ejercicio 1/KioskoDenuevo/src/kioskodenuevo/Producto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kioskodenuevo;
+
+/**
+ *
+ * @author Heresy
+ */
+
+/* a) Crear un tipo de registro llamado Producto que posea:
+nombre del producto (texto), su marca, su precio (número
+decimal) y la cantidad que hay en stock actualmente
+(número entero). */
+
+public class Producto {
+    String nombre;
+    String marca;
+    double precio;
+    int stock;
+}
