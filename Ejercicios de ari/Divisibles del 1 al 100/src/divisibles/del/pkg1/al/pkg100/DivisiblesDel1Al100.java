9c27aef4085be9d15d739107953e3786a1845768
Fernando <fernandolamasw@hotmail.com>
2018-09-22 11:15:26 -0300

    Divisible por 3 y 5 del 1 al 100
    Done!

---
+++ Ejercicios de ari/Divisibles del 1 al 100/src/divisibles/del/pkg1/al/pkg100/DivisiblesDel1Al100.java
@@ -0,0 +1,28 @@
+package divisibles.del.pkg1.al.pkg100;
+
+
+public class DivisiblesDel1Al100 {
+
+
+    public static void main(String[] args) {
+        
+                int divisiblesPorTres;
+                int divisibilesPorCinco;
+                int i;
+     
+                for(i = 0; i > 0; i++){
+        
+                    if(0 = (i MOD 3)){
+            
+            
+                 }
+        }
+            
+        }
+        
+    /*Quiero un programa que muestre cuales números enteros del 1 al 100 son divisibles por 3 y cuales son divisibles por 5.*/
+    
+        
+
+    
+}
