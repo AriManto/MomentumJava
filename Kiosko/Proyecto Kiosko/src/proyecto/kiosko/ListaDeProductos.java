9cfcb4169bdeca7b5a1eb54282671df202c15c1d
Fernando <fernandolamasw@hotmail.com>
2018-09-26 23:08:49 -0300

    Se hizo hasta el c

---
+++ Kiosko/Proyecto Kiosko/src/proyecto/kiosko/ListaDeProductos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.kiosko;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Heresy
+ */
+public class ListaDeProductos {
+    
+    ArrayList<Producto> lista;
+    
+    ListaDeProductos() {
+        this.lista = new ArrayList<Producto>();
+            }
+    
+}
--- Kiosko/Proyecto Kiosko/src/proyecto/kiosko/ProyectoKiosko.java
+++ Kiosko/Proyecto Kiosko/src/proyecto/kiosko/ProyectoKiosko.java
@@ -1,11 +1,41 @@
 package proyecto.kiosko;
 
+import java.util.Scanner;
+
 
 public class ProyectoKiosko {
 
 
     public static void main(String[] args) {
-
+        
+        
+        
     }
+    
+    public ListaDeProductos cargarLista(){
+        Producto nuevaLista = new Producto();
+        ListaDeProductos ingresados = new ListaDeProductos();
+        
+        Scanner ent = new Scanner(System.in);
+        
+        System.out.println("Ingrese nombre");
+        nuevaLista.nombre = ent.nextLine();
+        
+        System.out.println("Ingrese marca");
+        nuevaLista.marca = ent.nextLine();
+        
+        System.out.println("Ingrese precio");
+        nuevaLista.precio = ent.nextDouble();
+        ent.nextLine();
+        
+        System.out.println("Ingrese stock");
+        nuevaLista.stock = ent.nextInt();
+        ent.nextLine();
+        
+        ingresados.lista.add(nuevaLista);
+        
+        
+    return ingresados;
+            }
 
 }
